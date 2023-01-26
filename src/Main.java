693b1d9c7c0c23b66b5c256744376dfc5f3999d4
Jose Antonio Montero <josamontero@gmail.com>
2023-01-26 10:48:26 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado = 0;
+        resultado = suma(3,4, 10);
+        System.out.println(resultado);
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
